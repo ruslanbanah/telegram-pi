b660c5bf905eb5ca79bd279970db0a4848799043
Ruslan Banakh <ruslan@globalit.no>
2017-03-20 16:03:35 +0200

    init.

---
+++ index.js
@@ -0,0 +1,26 @@
+const TelegramBot = require('node-telegram-bot-api');
+const gpio = require('rpi-gpio');
+var config = require('config.json')('./config.json');
+
+const bot = new TelegramBot(config.token, {polling: true});
+
+var commands = (msg)=>{
+  if(msg.entities){
+    return msg.entities.map((ent)=>{
+      return msg.text.slice(ent.offset, ent.offset+ent.length)
+    })
+  }
+  return [msg.text]
+}
+
+bot.on('message', (msg) => {
+  let cmd = commands(msg);
+  console.log('Your commands : ', cmd)
+  bot.sendMessage(config.chatId, 'Your commands : ' + cmd.join(','));
+});
+
+// gpio.setup(7, gpio.DIR_IN, ()=>{
+//   gpio.read(7, function(err, value) {
+//       bot.sendMessage(config.chatId, 'Двері: ' + value);
+//   });
+// });
